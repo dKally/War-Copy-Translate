e6d3309ae9afc8210dcfb57b9a3a9235d4a6ea02
Daniel <110414111+dKally@users.noreply.github.com>
2023-10-20 16:27:23 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
-const {app, BrowserWindow, ipcMain, Menu, globalShortcut, dialog} = require('electron')
+const {app, BrowserWindow, ipcMain, Menu, globalShortcut, clipboard} = require('electron')
 const path = require('path')
+const fs = require('fs')
 
 let win
 
 const isDev = process.env.NODE_ENV !== undefined && process.env.NODE_ENV === "development"? true:false
 
 app.setName("Translate APP")
+
+let capturedClipboardText
 
 function createWindow(){
     win = new BrowserWindow({
@@ -34,6 +37,31 @@ function createWindow(){
     globalShortcut.register('Shift+I', () => {
         win.webContents.openDevTools()
     })
+
+    globalShortcut.register('CommandOrControl+Shift+C', () => {
+        try {
+            // Acessa diretamente a área de transferência usando clipboard
+            capturedClipboardText = clipboard.readText();
+            console.log('Texto da área de transferência capturado:', capturedClipboardText);
+
+            // Agora você pode salvar 'capturedClipboardText' no JSON e notificar a janela renderer, se necessário
+            fs.readFile('textos.json', 'utf-8', (err, data) => {
+                if (!err) {
+                    let textos = JSON.parse(data);
+                    textos.push(capturedClipboardText);
+                    fs.writeFile('textos.json', JSON.stringify(textos), (err) => {
+                        if (!err) {
+                            console.log('Copiado')
+                        } else {
+                            console.log('Deu erro')
+                        }
+                    });
+                }
+            });
+        } catch (error) {
+            console.error('Erro ao capturar o texto da área de transferência:', error);
+        }
+    });
 }
 
 app.whenReady().then(()=>{
@@ -62,3 +90,31 @@ app.on('will-quit', function () {
 const menuTemplate = [
 
 ]
+
+ipcMain.on('salvar-texto', (event, texto) => {
+    fs.readFile('textos.json', 'utf-8', (err, data) => {
+        let textos = [];
+        if (!err) {
+            textos = JSON.parse(data);
+        }
+        textos.push(texto);
+        fs.writeFile('textos.json', JSON.stringify(textos), (err) => {
+            if (!err) {
+                event.sender.send('texto-salvo', 'Texto salvo com sucesso.');
+            } else {
+                event.sender.send('texto-salvo', 'Erro ao salvar o texto.');
+            }
+        });
+    });
+});
+
+ipcMain.on('traduzir-texto', (event) => {
+    fs.readFile('textos.json', 'utf-8', (err, data) => {
+        if (!err) {
+            const textos = JSON.parse(data);
+            // Implemente aqui a lógica de tradução para o português
+            // e envie o texto traduzido de volta para a interface do usuário
+            event.sender.send('texto-traduzido', textos);
+        }
+    });
+});
